0fb58288a95b2ee24f86ef157354b66d79649d69
webdevaza <119423737+webdevaza@users.noreply.github.com>
2022-12-27 21:24:07 +0600

    Azadec26 (#2)
    * welcome modified
    
    * installed tailwindcss
    
    Co-authored-by: Azamat <azamailnet@mail.ru>

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Hello</title>
+    @vite('resources/css/app.css')
+    <title>Agenda</title>
 </head>
 <body>
-    <h1>Hi there my todo</h1>
+    <h1 class="text-3xl font-bold underline">Hi there my todo</h1>
 </body>
 </html>
