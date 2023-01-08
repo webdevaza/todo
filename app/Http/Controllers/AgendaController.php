535bf2a9402869b1ed327bb3b2f54d94088a5d1d
Azamat <azamailnet@mail.ru>
2023-01-08 21:52:48 +0600

    added remote quotes api

--- app/Http/Controllers/AgendaController.php
+++ app/Http/Controllers/AgendaController.php
@@ -13,18 +13,34 @@ class AgendaController extends Controller
 
         $todos = Agenda::where('done',0)->orderBy('updated_at','desc')->get();
         $dones = Agenda::where('done',1)->orderBy('updated_at','desc')->get();
-        $merged = $todos->merge($dones);
-        
-        
-        
+        $merged = $todos->merge($dones);  
+               
         return view('bootstrap.pages.tasks', ['todos'=>$merged]);
+    }
+
+    public function completed() {
+        // $todos = Agenda::latest()->paginate(8);
+        // return view('bootstrap.pages.tasks', ['todos'=>$todos]);
+
+        $dones = Agenda::where('done',1)->orderBy('updated_at','desc')->get();
+        
+        return view('bootstrap.pages.tasks', ['todos'=>$dones]);
+    }
+
+    public function toComplete() {
+        // $todos = Agenda::latest()->paginate(8);
+        // return view('bootstrap.pages.tasks', ['todos'=>$todos]);
+
+        $todos = Agenda::where('done',0)->orderBy('updated_at','desc')->get();
+        
+        return view('bootstrap.pages.tasks', ['todos'=>$todos]);
     }
 
     public function store(Request $request, Agenda $task) {
         $request->validate(['todo'=>'required']);
         $task->to_do = $request->todo;
         $task->save();
-        return redirect('tasks');
+        return redirect('tasks')->with('success','Task stored!');
     }
     
 
@@ -32,7 +48,7 @@ class AgendaController extends Controller
         $itemToDelete = Agenda::find($id);
         $itemToDelete->delete();
 
-        return redirect('tasks');
+        return redirect('tasks')->with('success','Task removed!');
     }
 
     public function update(Request $request, $id) {
@@ -42,7 +58,11 @@ class AgendaController extends Controller
         
         $itemToUpdate->save();
         // return redirect('tasks');
-        return back();
+        if($itemToUpdate->done == 1) {
+            return back()->with('success','Task completed!');
+        } else {
+            return back()->with('success','Task incomplete again!');
+        }
     }
     
 }
--- resources/views/bootstrap/includes/header.blade.php
+++ resources/views/bootstrap/includes/header.blade.php
@@ -1,19 +1,19 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-      <a class="navbar-brand" href="/tasks">All tasks</a>
+      <a class="navbar-brand" href="/">Home</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="/">Home</a>
+            <a class="nav-link active" aria-current="page" href="/tasks">All tasks</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Completed</a>
+            <a class="nav-link" href="{{route('tasks.completed')}}">Completed</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">To complete</a>
+            <a class="nav-link" href="{{route('tasks.toComplete')}}">To complete</a>
           </li>
         </ul>
         <ul class="navbar-nav ms-auto">
